21633cfebb1911e225d0b674338d44d794383084
Laura Gong <lkgong09@gmail.com>
2019-10-16 00:20:50 -0700

    Added Ch3 Exercises 3 and 4

---
+++ Ch3/AccelC++-Exercise2-4.cpp
@@ -0,0 +1,64 @@
+// AccelC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//  This version of the program writes the blank spaces in the blank rows all at once, instead of
+// character by character.
+
+#include <iostream>
+#include <string>  // Add standard library string header file
+
+int main() {
+
+	std::cout << "Please enter your first name: ";
+	std::string name;
+	std::cin >> name;
+	std::cout << "How much spacing would you like between the greeting and the border?: ";
+	int pad;
+	std::cin >> pad;
+	std::cout << std::endl; // Extra line
+
+	// Build out the greeting string
+	const std::string greeting = "Hello, " + name + "!";
+
+	// Set variables for row and column size
+	const int row = 3 + 2 * pad; // 1 row for greeting string + 2 blank spaces (pad) + 2 stars
+	// Use string::size_type when dealing with string sizes- safe practice
+	const std::string::size_type column = greeting.size() + 2 * pad + 2; // Size of greeting string + 2 blank spaces (pad) + 2 stars
+
+	// Loop invariant = r is the row number that we are currently writing characters at
+	for (int r = 0; r < row; r++)
+	{
+		std::string::size_type c = 0;
+		// Loop invariant = c is the column number that we are currently writing characters at
+		while (c != column)
+		{
+			// If we are at the location to start writing our greeting string, then write it
+			if (r == pad + 1 && c == pad + 1)
+			{
+				std::cout << greeting;
+				c += greeting.size();
+			}
+			else {
+				// If the character is on the border, then it should be a "*"
+				if (r == 0 || r == row - 1 || c == 0 || c == column - 1) {
+					std::cout << "*";
+					c++;
+				}
+				else {
+					// Write blanks up until last "*" at the end of row, unless it's the greeting row
+					if (r != pad + 1)
+					{
+						std::string blank_string(greeting.size() + pad * 2, ' ');
+						std::cout << blank_string;
+						c += greeting.size() + pad * 2;
+					}
+					else {
+						// Write one blank character if it's the greeting row
+						std::cout << " ";
+						c++;
+					}
+				}
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
