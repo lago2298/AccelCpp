b9e26e256e8e1da99f7729f72d64f5c6724ddf63
Laura Gong <lkgong09@gmail.com>
2019-10-17 17:32:58 -0700

    Initial Ch4

---
+++ Ch4/Ch4.cpp
@@ -0,0 +1,36 @@
+//
+//  Ch4.cpp
+//
+
+#include <iomanip>  // needed for setprecision
+#include <ios>  // needed for streamsize
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Function to calculate the median homework grade
+
+// Function to calculate the final grade
+
+// Function to read in homework grades
+
+int main()
+{
+    // Ask for student name
+    cout << "What is your name?: ";
+    string name;
+    cin >> name;
+    
+    // Ask for midterm and final grades
+    cout << endl;
+    double midterm, final;
+    cout << "Please enter your midterm and final grades: ";
+    cin >> midterm >> final;
+    
+    
+    
+    
+    return 0;
+}
